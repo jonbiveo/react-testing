7cbe15e03e34a682e897403d26d5ca3496eccdd5
Jonbiveo <logantedmonds@gmail.com>
2022-09-02 01:10:22 -0500

    Changed styles

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -3,11 +3,11 @@ import React, { Component } from 'react';
 class Counter extends Component {
   state = {
     count: 0,
+    tags: ['tag1', 'tag2', 'tag3'],
   };
 
   render() {
-    let classes = 'badge m-2 bg-';
-    classes += this.state.count === 0 ? 'warning' : 'primary';
+    let classes = this.getBadgeClasses();
 
     return (
       <div>
@@ -15,8 +15,19 @@ class Counter extends Component {
           {this.formatCount()}
         </span>
         <button className='btn btn-secondary btn-sm'>Increment</button>
+        <ul>
+          {this.state.tags.map((tag) => (
+            <li key={tag}>{tag}</li>
+          ))}
+        </ul>
       </div>
     );
+  }
+
+  getBadgeClasses() {
+    let classes = 'badge m-2 bg-';
+    classes += this.state.count === 0 ? 'warning' : 'primary';
+    return classes;
   }
 
   formatCount() {
